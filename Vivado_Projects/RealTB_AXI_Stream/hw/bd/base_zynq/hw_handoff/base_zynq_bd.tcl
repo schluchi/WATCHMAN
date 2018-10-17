a8aa0bdeff4cd409c53bda9da051b1da5c38a32c
schluchi <anthony.schluchin@gmail.com>
2018-10-17 13:16:05 -1000

    -

--- Vivado_Projects/RealTB_AXI_Stream/hw/bd/base_zynq/hw_handoff/base_zynq_bd.tcl
+++ Vivado_Projects/RealTB_AXI_Stream/hw/bd/base_zynq/hw_handoff/base_zynq_bd.tcl
@@ -58,7 +58,7 @@ set run_remote_bd_flow 1
 if { $run_remote_bd_flow == 1 } {
   # Set the reference directory for source file relative paths (by default 
   # the value is script directory path)
-  set origin_dir ./GitProjects/WATCHMAN/Vivado_Projects/RealTB_AXI_Stream/hw/bd
+  set origin_dir ./bd
 
   # Use origin directory path location variable, if specified in the tcl shell
   if { [info exists ::origin_dir_loc] } {
