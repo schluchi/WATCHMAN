a34f6734f96d2ffe8196f7c49d1dea5c803d2ef7
schluchi <schluchi@hawaii.edu>
2019-01-25 08:24:49 -1000

    -

--- Vivado_Projects/TARGETC_Prototype/sdk/UDP/Debug/src/i2c_access.d
+++
@@ -1,7 +0,0 @@
-src/i2c_access.o src/i2c_access.o: ../src/i2c_access.c \
- ../../UDP_bsp/ps7_cortexa9_0/include/xparameters.h \
- ../../UDP_bsp/ps7_cortexa9_0/include/xparameters_ps.h
-
-../../UDP_bsp/ps7_cortexa9_0/include/xparameters.h:
-
-../../UDP_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
--- Vivado_Projects/TARGETC_Prototype/sdk/UDP/Debug/src/si5324.d
+++
@@ -1,7 +0,0 @@
-src/si5324.o src/si5324.o: ../src/si5324.c \
- ../../UDP_bsp/ps7_cortexa9_0/include/xparameters.h \
- ../../UDP_bsp/ps7_cortexa9_0/include/xparameters_ps.h
-
-../../UDP_bsp/ps7_cortexa9_0/include/xparameters.h:
-
-../../UDP_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
