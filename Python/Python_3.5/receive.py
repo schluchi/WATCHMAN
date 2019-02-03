582e90ab1aedad6fb86d0e02a3ba5a9574d20b5f
schluchi <schluchi@hawaii.edu>
2019-02-03 12:21:26 -1000

    -

--- Python/Python_3.5/receive.py
+++ Python/Python_3.5/receive.py
@@ -34,15 +34,17 @@ class Watchman_graphic_window():
         ## Contain the port number for UDP communication
         self.UDP_PORT = 8
         ## Contain the number of hit for every channel, list ploted in hitmap graphic
-        self.hit_per_ch = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.hit_per_ch = [9,3,7,7,1,2,6,8,4,5,6,6,2,1,0,4]#[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ## Contain the 5 different amplitudes repartition for every channel, list plot in amplitude graphic
         self.amplitude = []
         for i in range(16):
             self.amplitude.append([0, 0, 0, 0, 0])
+        self.amplitude[0]=[0,1,4,2,0]
         ## Contain the 5 different times repartition for every channel, list plot in time graphic
         self.time = []
         for i in range(16):
             self.time.append([0, 0, 0, 0, 0])
+        self.time[0]=[1,0,4,2,2]
         ## Contain every channel, used for the list box and to update the graphic's title
         self.combolist = ["CH0", "CH1", "CH2", "CH3", "CH4", "CH5", "CH6", "CH7", "CH8", "CH9", "CH10", "CH11", "CH12", "CH13", "CH14", "CH15"]
         ## Contain which channel is currently choosed by the user
@@ -61,54 +63,70 @@ class Watchman_graphic_window():
         # initialization
         self.init_window()
         self.init_UDP_connection()
-        self.open_file()
+        self.__file = open("data.bin", "wb")
         self.run_flag = True
 
         t=Thread(target=self.recv_data, args=())
         t.start()
         self.thread_list.append(t)
         self.master.after(1000,self.plot_int)
-
-    ## Method called every second which update the graphics with the new datas
-    # and show the channel selected by the user
+    
+    ## Method to initialize the windows and create its graphical objects
     # @param self : The object pointer
-    def plot_int(self):
-        if(self.run_flag): # test if the app need to end
-            with self.lock_graph:   # acquire the lock to access the graphics object
-                start = time.time()
-                # update value of graph hitmap for every channels
-                for k in range(len(self.hit_per_ch)):
-                    self.__graph_hit.patches[k].set_height(self.hit_per_ch[k])
-                self.__subplot_hit.set_ylim([0, max(max(self.hit_per_ch)*1.1,1)]) # update y scale
-                # update value of graph amplitude for channel selected by the user
-                for k in range(len(self.amplitude[self.ch_selected])):
-                    self.__graph_amp.patches[k].set_height(self.amplitude[self.ch_selected][k])
-                self.__subplot_amp.set_ylim([0, max(max(self.amplitude[self.ch_selected])*1.1,1)]) # update y scale
-                self.__subplot_amp.set_title(self.combolist[self.ch_selected]) # change title in function of channel selected
-                # update value of graph time for channel selected by the user
-                for k in range(len(self.time[self.ch_selected])):
-                    self.__graph_time.patches[k].set_height(self.time[self.ch_selected][k])
-                self.__subplot_time.set_ylim([0, max(max(self.time[self.ch_selected])*1.1,1)]) # update y scale
-                self.__subplot_time.set_title(self.combolist[self.ch_selected]) # change title in function of channel selected
-                # draw all graphics
-                self.__canvas.draw()
-                self.__canvas.get_tk_widget().update_idletasks()
-                end = time.time()
-                # release the lock
-            #print("frame received : " + str(self.count)+" | thread engaged : " + str(len(self.thread_list)) + " | time : " + str(end-start))
-            # check in the thread's list if some have ended and can be removed
-            length=len(self.thread_list)
-            k=0
-            while(k<length):
-                if(not self.thread_list[k].is_alive()):
-                    del self.thread_list[k]
-                    length -= 1
-                else:
-                    k += 1
-            # build up the next call of this method
-            self.master.after(1000,self.plot_int)
-        else:
-            print("end of plot int.", file=sys.stderr)
+    def init_window(self):
+        self.master.title("Watchman - data") # change window's title
+        self.master.resizable(width=FALSE, height=FALSE) # set up window's size and not resizable by the user
+        self.master.geometry("1600x800+250+150")
+        # create the combox the selected which channel is represented by the graphics
+        self.__combo = ttk.Combobox(self.master, values=self.combolist) 
+        self.__combo.bind("<<ComboboxSelected>>", self.combo_callback) # attach a callback when user change channel
+        self.__combo.current(0) # on application start, selected first element of combolist
+        self.__combo.pack()
+        # create the figure which contains the three subplot
+        self.__figure = plt.figure() 
+        # create hitmap graphic
+        self.__subplot_hit = self.__figure.add_subplot(122) 
+        cat_grah_hitmap = ('CH0', 'CH1', 'CH2', 'CH3', 'CH4', 'CH5', 'CH6', 'CH7', 'CH8', 'CH9', 'CH10', 'CH11', 'CH12', 'CH13', 'CH14', 'CH15') # categories name
+        y_pos_graph_hitmap = np.arange(len(cat_grah_hitmap)) # vector with a length egal to the number of categories
+        self.__graph_hit = self.__subplot_hit.bar(y_pos_graph_hitmap, self.hit_per_ch, align='center', alpha=0.5) # type bar
+        self.__subplot_hit.set_xticks(y_pos_graph_hitmap) # where to stick categories
+        self.__subplot_hit.set_xticklabels(cat_grah_hitmap) # set the categories name
+        self.__subplot_hit.set_ylabel('Hits') # set the y axe's label
+        self.__subplot_hit.set_title('Hitmap') # set the graphic's title
+        # create amplitude graphic
+        self.__subplot_amp = self.__figure.add_subplot(221)
+        cat_graph_ampl = ('[0;205[', '[205;410[', '[410;615[', '[615;820[', '[820;1024]')
+        y_pos_graph_ampl = np.arange(len(cat_graph_ampl))
+        self.__graph_amp = self.__subplot_amp.bar(y_pos_graph_ampl, self.amplitude[0], align='center', alpha=0.5)
+        self.__subplot_amp.set_xticks(y_pos_graph_ampl)
+        self.__subplot_amp.set_xticklabels(cat_graph_ampl)
+        self.__subplot_amp.set_xlabel('Amplitude [V]')
+        self.__subplot_amp.set_title('CH0')
+        # create time graphic
+        self.__subplot_time = self.__figure.add_subplot(223)
+        cat_graph_time = ('[0;32[', '[32;64[', '[64;96[', '[96;128]', 'to long')
+        y_pos_graph_time = np.arange(len(cat_graph_time))
+        self.__graph_time = self.__subplot_time.bar(y_pos_graph_time, self.time[0], align='center', alpha=0.5)
+        self.__subplot_time.set_xticks(y_pos_graph_time)
+        self.__subplot_time.set_xticklabels(cat_graph_time)
+        self.__subplot_time.set_xlabel('Time [ns]')
+        self.__subplot_time.set_title('CH0')
+        plt.subplots_adjust(left=0.05, bottom=0.075, right=0.95, top=0.95, wspace=0.1, hspace=0.25)
+        # create a canvas, used to update the graphics, without recreating the complete object
+        self.__canvas = FigureCanvasTkAgg(self.__figure, master=self.master) 
+        self.__canvas.draw()
+        self.__canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
+
+    ## Method to initialize the UDP connection
+    # @param self : The object pointer
+    def init_UDP_connection(self):
+        ## Socket object used to established the UDP connection with the zynq
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2097152)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 131072) # change the size of the socket buffer
+        #print("sock buz:", self.sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF))
+        self.sock.bind(('', self.UDP_PORT))
+        self.sock.settimeout(0.1) # method sock.recvfrom return after maximum 0.1sec if no data are received
 
     ## Method thread to group the data received by UDP and prepare them for processing (running all the time)
     # @param self : The object pointer
@@ -239,72 +257,52 @@ class Watchman_graphic_window():
         del list_adress[:]
         print("data received: count = "+str(self.count)+" | lostcount = "+str(self.lostcnt))
 
-    ## Method to initialize the windows and create its graphical objects
+    ## Method called every second which update the graphics with the new datas
+    # and show the channel selected by the user
     # @param self : The object pointer
-    def init_window(self):
-        self.master.title("Watchman - data") # change window's title
-        self.master.resizable(width=FALSE, height=FALSE) # set up window's size and not resizable by the user
-        self.master.geometry("1600x800+250+150")
-        # create the combox the selected which channel is represented by the graphics
-        self.__combo = ttk.Combobox(self.master, values=self.combolist) 
-        self.__combo.bind("<<ComboboxSelected>>", self.combo_callback) # attach a callback when user change channel
-        self.__combo.current(0) # on application start, selected first element of combolist
-        self.__combo.pack()
-        # create the figure which contains the three subplot
-        self.__figure = plt.figure() 
-        # create hitmap graphic
-        self.__subplot_hit = self.__figure.add_subplot(122) 
-        cat_grah_hitmap = ('CH0', 'CH1', 'CH2', 'CH3', 'CH4', 'CH5', 'CH6', 'CH7', 'CH8', 'CH9', 'CH10', 'CH11', 'CH12', 'CH13', 'CH14', 'CH15') # categories name
-        y_pos_graph_hitmap = np.arange(len(cat_grah_hitmap)) # vector with a length egal to the number of categories
-        self.__graph_hit = self.__subplot_hit.bar(y_pos_graph_hitmap, self.hit_per_ch, align='center', alpha=0.5) # type bar
-        self.__subplot_hit.set_xticks(y_pos_graph_hitmap) # where to stick categories
-        self.__subplot_hit.set_xticklabels(cat_grah_hitmap) # set the categories name
-        self.__subplot_hit.set_ylabel('Hits') # set the y axe's label
-        self.__subplot_hit.set_title('Hitmap') # set the graphic's title
-        # create amplitude graphic
-        self.__subplot_amp = self.__figure.add_subplot(221)
-        cat_graph_ampl = ('[0;205[', '[205;410[', '[410;615[', '[615;820[', '[820;1024]')
-        y_pos_graph_ampl = np.arange(len(cat_graph_ampl))
-        self.__graph_amp = self.__subplot_amp.bar(y_pos_graph_ampl, self.amplitude[0], align='center', alpha=0.5)
-        self.__subplot_amp.set_xticks(y_pos_graph_ampl)
-        self.__subplot_amp.set_xticklabels(cat_graph_ampl)
-        self.__subplot_amp.set_xlabel('Amplitude [V]')
-        self.__subplot_amp.set_title('CH0')
-        # create time graphic
-        self.__subplot_time = self.__figure.add_subplot(223)
-        cat_graph_time = ('[0;32[', '[32;64[', '[64;96[', '[96;128]', 'to long')
-        y_pos_graph_time = np.arange(len(cat_graph_time))
-        self.__graph_time = self.__subplot_time.bar(y_pos_graph_time, self.time[0], align='center', alpha=0.5)
-        self.__subplot_time.set_xticks(y_pos_graph_time)
-        self.__subplot_time.set_xticklabels(cat_graph_time)
-        self.__subplot_time.set_xlabel('Time [ns]')
-        self.__subplot_time.set_title('CH0')
-        plt.subplots_adjust(left=0.05, bottom=0.075, right=0.95, top=0.95, wspace=0.1, hspace=0.25)
-        # create a canvas, used to update the graphics, without recreating the complete object
-        self.__canvas = FigureCanvasTkAgg(self.__figure, master=self.master) 
-        self.__canvas.draw()
-        self.__canvas.get_tk_widget().pack(side=BOTTOM, fill=BOTH, expand=True)
-
+    def plot_int(self):
+        if(self.run_flag): # test if the app need to end
+            with self.lock_graph:   # acquire the lock to access the graphics object
+                start = time.time()
+                # update value of graph hitmap for every channels
+                for k in range(len(self.hit_per_ch)):
+                    self.__graph_hit.patches[k].set_height(self.hit_per_ch[k])
+                self.__subplot_hit.set_ylim([0, max(max(self.hit_per_ch)*1.1,1)]) # update y scale
+                # update value of graph amplitude for channel selected by the user
+                for k in range(len(self.amplitude[self.ch_selected])):
+                    self.__graph_amp.patches[k].set_height(self.amplitude[self.ch_selected][k])
+                self.__subplot_amp.set_ylim([0, max(max(self.amplitude[self.ch_selected])*1.1,1)]) # update y scale
+                self.__subplot_amp.set_title(self.combolist[self.ch_selected]) # change title in function of channel selected
+                # update value of graph time for channel selected by the user
+                for k in range(len(self.time[self.ch_selected])):
+                    self.__graph_time.patches[k].set_height(self.time[self.ch_selected][k])
+                self.__subplot_time.set_ylim([0, max(max(self.time[self.ch_selected])*1.1,1)]) # update y scale
+                self.__subplot_time.set_title(self.combolist[self.ch_selected]) # change title in function of channel selected
+                # draw all graphics
+                self.__canvas.draw()
+                self.__canvas.get_tk_widget().update_idletasks()
+                end = time.time()
+                # release the lock
+            #print("frame received : " + str(self.count)+" | thread engaged : " + str(len(self.thread_list)) + " | time : " + str(end-start))
+            # check in the thread's list if some have ended and can be removed
+            length=len(self.thread_list)
+            k=0
+            while(k<length):
+                if(not self.thread_list[k].is_alive()):
+                    del self.thread_list[k]
+                    length -= 1
+                else:
+                    k += 1
+            # build up the next call of this method
+            self.master.after(1000,self.plot_int)
+        else:
+            print("end of plot int.", file=sys.stderr)
+    
     ## Method callback called when the user change the channel selected in the list box
     # @param self : The object pointer
     # @param event : Unused
     def combo_callback(self, event):
         self.ch_selected = self.__combo.current()
-
-    ## Method to initialize the UDP connection
-    # @param self : The object pointer
-    def init_UDP_connection(self):
-        ## Socket object used to established the UDP connection with the zynq
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2097152)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 131072) # change the size of the socket buffer
-        #print("sock buz:", self.sock.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF))
-        self.sock.bind(('', self.UDP_PORT))
-        self.sock.settimeout(0.1) # method sock.recvfrom return after maximum 0.1sec if no data are received
-
-    ## Method to open the binary file to save the received data in
-    def open_file(self):
-        self.__file = open("data.bin", "wb")
 
     ## Method callback for the "WM_DELETE_WINDOW" event which quit the application
     # @param self : The object pointer
